b988b98a71a3b287803963577b2fcf1f4e9514d7
Кирилл <vil.rik@mail.ru>
2022-01-10 15:26:17 +0300

    - add work with image - add button auth (but not realization)

--- src/main/java/testSwing/Main.java
+++ src/main/java/testSwing/Main.java
@@ -1,10 +1,13 @@
 package testSwing;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         SimpleGUI app = new SimpleGUI();
         app.setVisible(true);
+
     }
 }
--- src/main/java/testSwing/SimpleGUI.java
+++ src/main/java/testSwing/SimpleGUI.java
@@ -2,55 +2,73 @@ package testSwing;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
 public class SimpleGUI extends JFrame {
-    private final JButton register = new JButton("Регистрация");
 
 
-    private final JLabel labelName = new JLabel("введите имя:");
     private final JTextField nameTextFiled = new JTextField("");
 
-    private final JLabel labelSecondName = new JLabel("введите фамилию:");
     private final JTextField secondNameTextField = new JTextField("");
 
-    private final JLabel labelAge = new JLabel("введите возраст:");
     private final JTextField ageTextField = new JTextField("");
 
-    private final JLabel labelPassword = new JLabel("введите пароль:");
     private final JPasswordField passwordField = new JPasswordField("");
 
 
-
-
-    public SimpleGUI() {
+    public SimpleGUI() throws IOException {
         super("Register user");
         this.setBounds(1000,100,350, 250);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Container container = this.getContentPane();
-        container.setLayout(new GridLayout(0,2,1,1));
 
+        Image image = ImageIO.read(new File("FIk5-qXUUAAn7xm.jpg")).getScaledInstance(50,150, Image.SCALE_SMOOTH);
+
+
+        JLabel imageLabel = new JLabel();
+        imageLabel.setIcon(new ImageIcon(image));
+
+        Container container = this.getContentPane();
+        container.setLayout(new GridLayout(0, 2 , 0, 0));
+
+        JLabel labelName = new JLabel("введите имя:");
         container.add(labelName);
         container.add(nameTextFiled);
 
+        JLabel labelSecondName = new JLabel("введите фамилию:");
         container.add(labelSecondName);
         container.add(secondNameTextField);
 
+        JLabel labelAge = new JLabel("введите возраст:");
         container.add(labelAge);
         container.add(ageTextField);
 
+        JLabel labelPassword = new JLabel("введите пароль:");
         container.add(labelPassword);
         container.add(passwordField);
 
-
+        JButton auth = new JButton("Авторизация");
+        container.add(auth);
+        JButton register = new JButton("Регистрация");
         container.add(register);
 
+//        container.add(imageLabel);
 
 
         register.addActionListener(new ActionListenerButton());
+        auth.addActionListener(new AuthListener());
 
+    }
+    class AuthListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Вы были авторизованы");
+        }
     }
 
     class ActionListenerButton implements ActionListener{
